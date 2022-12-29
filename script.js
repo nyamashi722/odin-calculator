6fe49dbe70606dad5633dfd348883960a7c6d230
nyamashi722 <yamashiro.nao@hotmail.com>
2022-12-29 17:02:17 -0500

    fix various bugs such as being able to press = twice, operator buttons operating when not supposed to, being able to input the next number on the answer screen

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const topDisplay = document.querySelector(".top-display")
 const bottomDisplay = document.querySelector(".bottom-display")
 const clear = document.querySelector(".clear")
 const equals = document.querySelector(".equals")
-
+const decimal = document.querySelector(".decimal")
 
 let a;
 let b;
@@ -34,52 +34,138 @@ function divide(a, b) {
 function operate(a, b, operatorSign) {
     if (operatorSign === "+") {
         answer = add(a, b);
+        bottomDisplay.textContent = answer;
         return answer;
     } else if (operatorSign === "-") {
         answer = subtract(a, b);
+        bottomDisplay.textContent = answer;
         return answer;
     } else if (operatorSign === "*") {
         answer = multiply(a, b);
+        bottomDisplay.textContent = answer;
         return answer;
     } else if (operatorSign === "/") {
         answer = divide(a, b);
+        bottomDisplay.textContent = answer;
         return answer;
     };
 };
 
+//selected = next number will be "b"
+//marked = to remove current bottomDisplay numbers
+//primed = new answer 
+
+function buttonAOne(e) {
+    a = e.target.value;
+    bottomDisplay.textContent = a;
+    a = parseFloat(bottomDisplay.textContent);
+    bottomDisplay.removeAttribute("data-marked")
+    bottomDisplay.setAttribute("data-primed", "true")
+    bottomDisplay.removeAttribute("data-answer")
+}
+
+function buttonATwo(e) {
+    a = e.target.value;
+    bottomDisplay.textContent += a;
+    a = parseFloat(bottomDisplay.textContent);
+}
+
+
+function buttonBOne(e) {
+    b = e.target.value;
+    bottomDisplay.textContent = b;
+    b = parseFloat(bottomDisplay.textContent);
+    bottomDisplay.removeAttribute("data-marked")
+    bottomDisplay.setAttribute("data-primed", "true")
+}
+
+function buttonBTwo(e) {
+    b = e.target.value;
+    bottomDisplay.textContent += b;
+    b = parseFloat(bottomDisplay.textContent);
+    bottomDisplay.setAttribute("data-primed", "true")
+    bottomDisplay.removeAttribute("data-marked")
+}
+
 numbers.forEach((number) => {
     number.addEventListener("click", (e) => {
-        if (e.target.dataset.selected && bottomDisplay.dataset.marked) {
-            b = e.target.value;
-            bottomDisplay.textContent = b;
-            b = parseFloat(bottomDisplay.textContent);
-            bottomDisplay.removeAttribute("data-marked")
+        if (bottomDisplay.dataset.answer) {
+            return false;
+        } else if (e.target.dataset.selected && bottomDisplay.dataset.marked && bottomDisplay.textContent) {
+            if (bottomDisplay.textContent == "0") {
+                bottomDisplay.textContent = "";
+                buttonBOne(e)
+            } else {
+                buttonBOne(e)
+            }
         } else if (e.target.dataset.selected) {
-            b = e.target.value;
-            bottomDisplay.textContent += b;
-            b = parseFloat(bottomDisplay.textContent);
+            if (bottomDisplay.textContent == "0") {
+                bottomDisplay.textContent = "";
+                buttonBTwo(e)
+            } else {
+                buttonBTwo(e)
+            }
+        } else if(bottomDisplay.textContent && bottomDisplay.dataset.marked) {
+            if (bottomDisplay.textContent == "0") {
+                bottomDisplay.textContent = "";
+                buttonAOne(e)
+            } else {
+                buttonAOne(e)
+            }
         } else {
-            a = e.target.value;
-            bottomDisplay.textContent += a;
-            a = parseFloat(bottomDisplay.textContent);
+            if (bottomDisplay.textContent == "0") {
+                bottomDisplay.textContent = "";
+                buttonATwo(e)
+            } else {
+                buttonATwo(e)
+            }
         }
     })
 })
 
 operators.forEach((operator) => {
     operator.addEventListener("click", (e) => {
-        operatorSign = e.target.value;
-        for (let i = 0; i < numbers.length; i++) {
-            numbers[i].setAttribute("data-selected", "true")
+        if (bottomDisplay.dataset.primed && bottomDisplay.dataset.marked) {
+            operatorSign = e.target.value;
+            for (let i = 0; i < numbers.length; i++) {
+                numbers[i].setAttribute("data-selected", "true")
+            }
+            bottomDisplay.setAttribute("data-marked", "true")
+            bottomDisplay.removeAttribute("data-answer")
+            topValue = a + " " + e.target.innerHTML;
+            topDisplay.textContent = topValue;
+            bottomDisplay.textContent = "";
+        } else if (bottomDisplay.dataset.primed) {
+            if (b == 0 && operatorSign == "/") {
+                alert ("You can't divide by 0")
+            } else {
+                operate(a, b, operatorSign)
+                topValue += " " + b + " " + e.target.innerHTML;
+                topDisplay.textContent = topValue;
+                operatorSign = e.target.value;
+                bottomDisplay.setAttribute("data-marked", "true")
+                bottomDisplay.removeAttribute("data-answer")
+                a = answer;
+            }
+        } else {
+            operatorSign = e.target.value;
+            for (let i = 0; i < numbers.length; i++) {
+                numbers[i].setAttribute("data-selected", "true")
+            }
+            bottomDisplay.setAttribute("data-marked", "true")
+            bottomDisplay.removeAttribute("data-answer")
+            topValue = a + " " + e.target.innerHTML;
+            topDisplay.textContent = topValue;
+            bottomDisplay.textContent = "";
         }
-        bottomDisplay.setAttribute("data-marked", "true")
-        topValue = a + " " + e.target.innerHTML;
-        topDisplay.textContent = topValue;
     })
 })
 
 clear.addEventListener("click", () => {
     bottomDisplay.textContent = "";
+    bottomDisplay.removeAttribute("data-primed");
+    bottomDisplay.removeAttribute("data-marked")
+    bottomDisplay.removeAttribute("data-answer")
     topDisplay.textContent = "";
     a = "";
     b = "";
@@ -92,11 +178,24 @@ clear.addEventListener("click", () => {
 })
 
 equals.addEventListener("click", () => {
-    if (bottomDisplay.textContent == answer) {
+    if (bottomDisplay.dataset.answer) {
         topDisplay.textContent = topValue;
-    } else {
-        bottomDisplay.textContent = operate(a, b, operatorSign)
-        topValue += " " + b + " " + "=";
-        topDisplay.textContent = topValue; 
+        bottomDisplay.removeAttribute("data-primed");
+    } else if (!bottomDisplay.textContent) {
+        topDisplay.textContent = topValue;
+        bottomDisplay.removeAttribute("data-primed");
+    } else if (bottomDisplay.dataset.primed && bottomDisplay.dataset.marked) {
+        return false;
+    } else if (bottomDisplay.dataset.primed) {
+        if (b == 0 && operatorSign == "/") {
+            alert("You can't divide by 0, please select a different number")
+        } else {
+            operate(a, b, operatorSign)
+            topValue += " " + b + " " + "=";
+            topDisplay.textContent = topValue;
+            a = answer;
+            bottomDisplay.removeAttribute("data-primed");
+            bottomDisplay.setAttribute("data-answer", "true")
+        }
     }
 })
